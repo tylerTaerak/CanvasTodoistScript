af3b5460c7f168944926c94a51f89b624ca5512b
Tyler Conley <tyler.taerak.conley@gmail.com>
2021-05-18 17:05:33 -0600

    Added first working version of the script. Does not currently support OAuth2, and needs more working on the logging for the -v and -d tags

--- canvas_script.py
+++ canvas_script.py
@@ -2,34 +2,53 @@ from canvasapi import Canvas
 import todoist
 import json
 import logging
+
+
+logging.basicConfig(level=logging.WARNING)
 # TODO: make use of the logging library to add INFO markers when running the script, as well as any ERRORs
 # Additionally, use a -v/--verbose and a -d/--debug marker to administrate this
 
 class CanvasAutomate:
-    def __init__(self, api_tokens='./json/access_tokens.json', rw_path='./json/info.json'):e
+    def __init__(self, 
+            api_tokens='./json/access_tokens.json', 
+            rw_path='./json/info.json', 
+            verbose=False, 
+            debug=False):
         try:
             with open(api_tokens) as handle:
                 tokens = json.load(handle)
 
-            if token['canvas-api-access'] == [] or tokens['todoist-api-access'] == '':
+            if tokens['canvas-api-access'] == [] or tokens['todoist-api-access'] == '':
                 raise Exception("check your credentials in the ./json/access_tokens.json file")
             self.cv = Canvas(tokens['canvas-api-access'][0], tokens['canvas-api-access'][1])
             self.td = todoist.TodoistAPI(tokens['todoist-api-access'])
             self.td.sync()
             self.write_path = rw_path
+
+            if verbose:
+                logging.basicConfig(level=logging.INFO)
+            if debug:
+                logging.basicConfig(level=logging.DEBUG)
+
             with open(rw_path) as handle:
                 self.info = json.load(handle)
+
         except FileNotFoundError as e:
             raise FileNotFoundError("check ./json/access_tokens.json or ./json/info.json")
 
 
     def __del__(self):
-        with open(self.write_path, 'w') as handle:
-            json.dump(self.info, handle)
-
+        logging.info('Finishing up...')
+        self.td.commit()
+        try:
+            with open(self.write_path, 'w') as handle:
+                json.dump(self.info, handle)
+        except Exception:
+            print('deleted without saving info')
+        logging.info('Complete. Deleting CanvasAutomate Object')
 
     # the top level function of the class, which will handle all the adding of assignments to the todo list
-    def do_the_thing(self):
+    def add_all_assignments_to_todoist(self):
         self.add_course_projects()
         for i in self.current_courses:
             for j in i.get_assignments():
@@ -52,36 +71,57 @@ class CanvasAutomate:
     def add_course_projects(self):
         courses = self.current_courses
         for i in courses:
-            if i.id in courses.keys():
-                continue
+            if str(i.id) in self.info.keys():
+                proj_lst = [x['id'] for x in self.td.state['projects']]
+                if self.info[str(i.id)]['project_id'] in proj_lst:
+                    continue
+            logging.info(f'Adding Course: {i.name}')
+            print(f'Adding Course: {i.name}')
+            self.info[str(i.id)] = {'assignments': []}
             proj = self.td.projects.add(i.name, color=i.course_color)
             self.td.commit()
-            self.info[i.id]['project_id'] = proj.id
+            self.info[str(i.id)]['project_id'] = proj['id']
+            logging.info(f'Course: {i.name} added')
+            print(f'Course: {i.name} added')
 
 
     # Adds an assignment task to Todoist, then adds the assignment id to the info dict
     def add_asgn(self, course, asgn):
-        if not verify_asgn(self.verify_asgn(course, asgn):
+        if not self.verify_asgn(course, asgn):
             return False
-        prj = self.info[course.id]['project_id']
-        strf = assgn.due_at.strftime('%b %d')
-        if assgn.due_at.hour != 11 and assgn.due_at.minute != 59:
-            strf += assgn.due_at.strftime(' %I:%M %p')
-        self.td.items.add(asgn.name, due={'string': strf}, project_id=prj, description=asgn.id)
-        self.info[course.id]['assignments'].append(asgn.id)
+        logging.info(f'adding Assignment: {asgn.name}')
+        print(f'Adding Assignment: {asgn.name}')
+        prj = self.info[str(course.id)]['project_id']
+        strf = None
+        if asgn.due_at is not None:
+            strf = asgn.due_at.strftime('%b %d')
+            if assgn.due_at.hour != 11 and assgn.due_at.minute != 59:
+                strf += assgn.due_at.strftime(' %I:%M %p')
+        self.td.items.add(asgn.name, due={'string': strf}, project_id=prj)
+        self.info[str(course.id)]['assignments'].append(asgn.id)
+        logging.info(f'Assignment: {asgn.name} added')
+        print(f'Assignment: {asgn.name} added')
         return True
 
 
     # Verifies whether an assignment task has already been made in Todoist
     # Returns False if the assignment has been made previously, and True otherwise
     def verify_asgn(self, course, asgn):
-        course = self.info[course.id]
-        if asgn.id in course['assignments']:
+        course = self.info[str(course.id)]
+        if str(asgn.id) in course['assignments']:
             return False
         return True
 
 
 if __name__ == '__main__':
     import argparse
-    cv = CanvasAutomate()
+    parser = argparse.ArgumentParser(description='Transfer Canvas Assignments to Todoist')
+    parser.add_argument('--access_tokens', dest='api_access', default='./json/access_tokens.json')
+    parser.add_argument('--info_file', dest='info_file', default='./json/info.json')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true')
+    args = parser.parse_args()
+
+    cv = CanvasAutomate(api_tokens=args.api_access, rw_path=args.info_file, verbose=args.verbose, debug=args.debug)
+    cv.add_all_assignments_to_todoist()
     del cv
